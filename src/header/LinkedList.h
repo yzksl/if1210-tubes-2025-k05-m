b5f213f053ad90c070820c611a5a8b3d4cfd0620
Zaki Syauqi <nougenki_mika@tuta.io>
2025-05-07 18:32:31 +0700

    feature(adt-linkedlist): add most important linked list implementation and header
    - Beberapa variable dalam header di refactor untuk klaritas
    - Bugfix beberapa fungsi (parameter dulunya tidak sesuai)
    - Dibuat implementasi semua fungsi yang kira-kira akan dibutuhkan dalam waktu kedepan

--- src/header/LinkedList.h
+++ src/header/LinkedList.h
@@ -22,9 +22,9 @@
 typedef struct LinkedListNode {
     int id; // ID dari pasien
     char name[50]; // Nama dari pasien
-    struct LinkedListNode *next; // Address node berikutnya
+    struct LinkedListNode* next; // Address node berikutnya
 } LinkedListNode;
-
+ 
 typedef struct LinkedList {
     LinkedListNode *head; // Address node pertama
     int size; // Jumlah elemen (node) dalam linked list
@@ -38,6 +38,7 @@ typedef struct LinkedList {
 #define SIZE(l) (l).size
 #define ID(node) (node).id
 #define NAME(node) (node).name
+#define NEXT(node) (node).next
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor: create list kosong */
@@ -48,26 +49,36 @@ void CreateLL(LinkedList *l);
 LinkedListNode* CreateLLNode(int id, char name[50]);
 /* Mengirimkan pointer ke Node */ 
 /* Konstruktor: sambungkan address node yang diterima ke node sebelumnya */
-void PutNodeInLastLL(LinkedListNode* NodeAddress, LinkedList* l);
+void PutNodeInLastLL(LinkedList* l, LinkedListNode* NodeAddress);
 /* I.S. l terdefinisi, bisa kosong atau bisa sudah ada isi */
 /* F.S. l memiliki node yang di parameter di bagian tail */
 /* Konstruktor: sisihkan node ke posisi-pos dalam linked list */
-void PutNodeInBetweenLL(LinkedListNode* NodeAddress, LinkedList* l, int pos);
-/* I.S. l terdefinisi, memiliki setidaknya 2 node */
-/* F.S. l memiliki node yang di parameter sebagai node di posisi ke-pos */
+void PutNodeInIdxLL(LinkedList* l, LinkedListNode* NodeAddress, int idx);
+/* I.S. l terdefinisi, memiliki setidaknya 1 node */
+/* F.S. l memiliki node yang di parameter sebagai node di posisi ke-pos. Jika pos invalid (>size), maka akan keluar "POSISI TIDAK VALID". Jika idx = size, panggil function puTNodeInLastLL */
 
 /* ********** SELEKTOR (TAMBAHAN) ********** */
-LinkedListNode getLLNodeById(LinkedList l, int id);
+LinkedListNode* getLLNodeById(const LinkedList* l, int id);
 /* Mengirimkan data node dengan id tertentu */
-LinkedListNode getLLNodeByIdx(LinkedList l, int pos);
+LinkedListNode* getLLNodeByIdx(const LinkedList* l, int idx);
 /* Mengirimkan data node dengan ekuivalen indeks*/
-LinkedListNode getLLNodeByName(LinkedList l, char name[50]);
+LinkedListNode* getLLNodeByName(const LinkedList* l, char name[50]);
 /* Mengirimkan data node dengan nama tertentu */
 
 /* ********** TEST KOSONG ********** */
 boolean isLLEmpty(LinkedList l);
 /* Mengirimikan true jika kosong, false jika tidak */
-boolean isLLNodeEmpty(LinkedListNode node);
-/* Mengirimkan true jika kosong, false jika tidak */
+// boolean isLLNodeEmpty(LinkedListNode node);
+// /* Mengirimkan true jika kosong, false jika tidak */
+/* BELUM DIBUAT KARENA BINGUNG */
+
+/* ********** DEALOKATOR ********** */
+/* Dealokator: dealokasi memori dari seluruh linked list */
+void FreeLL(LinkedList* l);
+/* I.S. l terdefinisi, memiliki node-node */
+/* F.S. l kosong, header point to NULL, seluruh node didealokasi juga */
+void FreeNodeLL(LinkedList* l, LinkedListNode* node);
+/* I.S. l terdefinisi, memiliki setidaknya satu nilai */
+/* F.S. l terdefinisi, mungkin kosong, node sudah didealokasi */
 
 #endif
