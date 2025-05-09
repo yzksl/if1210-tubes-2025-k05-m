a8cf44510df10f7501e449cbdd094165ea432377
Aqeel Ghani <aqeel70106@gmail.com>
2025-05-09 13:11:53 +0700

    test: perubahan main.c untuk testing

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 // Include any necessary headers
 #include <stdio.h>
 #include <MainMenu.h>
+#include <ReadConfig.h>
 
 int main() {
     // Your code goes here
-    while(1){ // Untuk Testing bukan permanen
-    printf("Role : ");
-    scanf("%s", Role);
-    MainMenu();
-    }
+
+    ReadConfig();
+    // while(1){ // Untuk Testing bukan permanen
+    // printf("Role : ");
+    // scanf("%s", Role);
+    // MainMenu();
+    // }
     return 0;
 }
