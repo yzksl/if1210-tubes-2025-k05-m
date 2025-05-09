0bd0fbdea815d6db6baf3f45064c893e083e6790
Zaki Syauqi <105656648+yzksl@users.noreply.github.com>
2025-05-10 05:08:26 +0700

    feat(adt): Create Obat Header
    Created initial (temporary) obat file for the purpose of making stack adt

---
+++ src/header/Obat.h
@@ -0,0 +1,18 @@
+/* MODUL ADT SEDERHANA OBAT  */
+/* ... */
+
+#ifndef OBAT_H
+#define OBAT_H
+
+#include "Boolean.h"
+
+/*  Kamus Umum */
+/* ... */
+
+/* Definisi elemen dan koleksi objek */
+typedef struct {
+    int id;         /* id obat */
+    char name[50];  /* nama obat */
+} Obat;
+
+#endif
