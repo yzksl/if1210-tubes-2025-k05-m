33c7374ab7d3e6cc824db463b81878393cfad138
josrLlusi <jose.fernadosaragi@gmail.com>
2025-05-12 11:49:03 +0700

    Update Sementara F15

---
+++ antrian saya.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+void AntrianSaya(){
+    if(!Terdaftar){
+        printf("\nAnda belum terdaftar dalam antrian check-up!\n");
+        printf("Silakan daftar terlebih dahulu dengan command DAFTAR_CHECKUP.\n");
+    }
+    else{
+        printf("\nStatus antrian Anda:\n");
+        printf("Dokter: %s\n", );
+        printf("Ruangan: %s\n", );
+        printf("Posisi antrian: %d dari %d\n", ,);
+
+    }
+}
