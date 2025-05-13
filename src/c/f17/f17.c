1a5f0ab82f6255fa2314056ace4947099df22e0b
josrLlusi <jose.fernadosaragi@gmail.com>
2025-05-13 10:06:14 +0700

    F17 sementara

---
+++ src/c/f17/f17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "f17.h"
+
+void createStack(Stack* perut){
+    perut->top = NULL;
+}
+void pushStack(Stack* s, Obat o){
+    
+}
