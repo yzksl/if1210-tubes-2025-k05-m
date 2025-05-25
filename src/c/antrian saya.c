f607a53ab4d9209c3dfa3e5ce2d5bdeb55028eec
josrLlusi <jose.fernadosaragi@gmail.com>
2025-05-25 22:12:04 +0700

    buka ulang file

--- src/c/antrian saya.c
+++ src/c/antrian saya.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include "GlobalVariable.h"
+#include "antrian saya.h"
 
 
+
+
+bool cekAntrian(){
+    
+}
 void AntrianSaya(){
     if(!Terdaftar){
         printf("\nAnda belum terdaftar dalam antrian check-up!\n");
         printf("Silakan daftar terlebih dahulu dengan command DAFTAR_CHECKUP.\n");
     }
     else{
+        
         printf("\nStatus antrian Anda:\n");
         printf("Dokter: %s\n", nama_dokter);
         printf("Ruangan: %s\n", kode_ruangan);
---
+++ src/header/antrian saya.h
@@ -0,0 +1,7 @@
+#include "GlobalVariable.h"
+#include "Boolean.h"
+#include "Queue.h"
+#include "LinkedList.h"
+
+bool cekAntrian();
+void antrianSaya();
