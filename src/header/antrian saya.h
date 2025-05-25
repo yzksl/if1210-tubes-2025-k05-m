c9aa169378f50711c1e59bd31cef455a82d92a99
josrLlusi <jose.fernadosaragi@gmail.com>
2025-05-25 23:19:11 +0700

    85% f15 update

--- src/header/antrian saya.h
+++ src/header/antrian saya.h
@@ -3,5 +3,9 @@
 #include "Queue.h"
 #include "LinkedList.h"
 
-bool cekAntrian();
+boolean cekAntrian(Queue* q, int userId);
+/*Fungsi untuk mengecek apakah pasien ada dalam antrian*/
+int cariPosisiPasien(Queue* q, int userId);
+/*Fungsi untuk mencari posisi pasien dalam antrian*/
 void antrianSaya();
+/*Prosedur utama untuk mengecek urutan antrian*/
