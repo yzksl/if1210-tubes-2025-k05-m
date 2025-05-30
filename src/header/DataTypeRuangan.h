afd32ffba6f69f0e250710adfcb7e8e3ff97bd6b
Aqeel Ghani <aqeel70106@gmail.com>
2025-05-30 15:54:11 +0700

    refactor: mengubah adt id pasien dari array biasa menjadi adt queue

--- src/header/DataTypeRuangan.h
+++ src/header/DataTypeRuangan.h
@@ -7,7 +7,7 @@
 
 typedef struct {
     int idDokter; // id dokter
-    int idPasien[MAX_CAPACITY]; // Array berisi id pasien
+    Queue idPasien; // Pasien dalam ruangan
     int nEffPasien; // Jumlah pasien
     Queue idAntrian; // Antrian Pasien
 } DataTypeRuangan; // Tipe data untuk ruangan
