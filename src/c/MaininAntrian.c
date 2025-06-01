09d5dfc9e88249047c5ac400c642ca7cea47aa17
Aqeel Ghani <aqeel70106@gmail.com>
2025-06-01 13:01:04 +0700

    feat: Penambahan fungsi cancel antrian

--- src/c/MaininAntrian.c
+++ src/c/MaininAntrian.c
@@ -9,7 +9,7 @@ void skipAntrian(){
     else {
         int sizeQueue=globalDenahRumahSakit.kapasitasRuangan+lokasiRuangan.antrian;
         LinkedListNode* pasienYangSkip = globalDenahRumahSakit.Ruangan[lokasiRuangan.row][lokasiRuangan.column].idAntrian.front, *pasienSebelumYangSkip, *pasienTerakhirDalamRuangan;
-        for (int i=1; i<sizeQueue; i++){
+        for (int i=1; i<sizeQueue && pasienYangSkip->id != globalCurrentPatient->id; i++){
             pasienSebelumYangSkip = pasienYangSkip;
             if(i==globalDenahRumahSakit.kapasitasRuangan) pasienTerakhirDalamRuangan = pasienYangSkip;
             pasienYangSkip = pasienYangSkip->next;
@@ -31,16 +31,15 @@ void cancelAntrian(){
     else if (lokasiRuangan.antrian==-2) printf("\nAnda sudah berada di dalam ruangan %c%d bersama Dr. %s! Tidak bisa cancel antrian.\n\n", lokasiRuangan.row+'A', lokasiRuangan.column+1, getAccountName(globalDenahRumahSakit.Ruangan[lokasiRuangan.row][lokasiRuangan.column].idDokter, DATA_TYPE_DOCTOR));
     else {
         int sizeQueue=globalDenahRumahSakit.kapasitasRuangan+lokasiRuangan.antrian;
-        LinkedListNode* pasienYangSkip = globalDenahRumahSakit.Ruangan[lokasiRuangan.row][lokasiRuangan.column].idAntrian.front, *pasienSebelumYangSkip, *pasienTerakhirDalamRuangan;
-        for (int i=1; i<sizeQueue; i++){
-            pasienSebelumYangSkip = pasienYangSkip;
-            if(i==globalDenahRumahSakit.kapasitasRuangan) pasienTerakhirDalamRuangan = pasienYangSkip;
-            pasienYangSkip = pasienYangSkip->next;
+        LinkedListNode* pasienYangCancel = globalDenahRumahSakit.Ruangan[lokasiRuangan.row][lokasiRuangan.column].idAntrian.front, *pasienSebelumYangCancel;
+        for (int i=1; i<sizeQueue && pasienYangCancel->id != globalCurrentPatient->id; i++){
+            pasienSebelumYangCancel = pasienYangCancel;
+            pasienYangCancel = pasienYangCancel->next;
         }
-        pasienYangSkip->next=pasienTerakhirDalamRuangan->next;
-        pasienTerakhirDalamRuangan->next=pasienSebelumYangSkip->next;
-        pasienSebelumYangSkip->next=NULL;
+        pasienSebelumYangCancel->next=pasienYangCancel->next;
+        free(pasienYangCancel);
+        globalDenahRumahSakit.Ruangan[lokasiRuangan.row][lokasiRuangan.column].idAntrian.size-=1;
 
-        printf("\nAnda berhasil maju ke depan antrian Dr. %s di ruangan %c%d!\nPosisi antrian Anda sebelumnya: %d\nPosisi antrian Anda sekarang: 1\n\n", getAccountName(globalDenahRumahSakit.Ruangan[lokasiRuangan.row][lokasiRuangan.column].idDokter, DATA_TYPE_DOCTOR), lokasiRuangan.row+'A', lokasiRuangan.column+1, lokasiRuangan.antrian);
+        printf("\nAnda berhasil keluar dari antrian Dr. %s di ruangan %c%d!\n\n", getAccountName(globalDenahRumahSakit.Ruangan[lokasiRuangan.row][lokasiRuangan.column].idDokter, DATA_TYPE_DOCTOR), lokasiRuangan.row+'A', lokasiRuangan.column+1);
     }
 }
